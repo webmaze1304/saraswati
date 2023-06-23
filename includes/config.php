431691bcb122fd8ce596a7a7d2800fcafc090d7c
webmaze1304 <mazeweb12@gmail.com>
2023-06-23 23:07:21 +0530

    Template change

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$server_name = "localhost";
-$username = "root";
-$password = "";
-$db_name = "sps";
-
-$conn = mysqli_connect($server_name, $username, $password, $db_name);
--- includes/scripts.php
+++
@@ -1,7 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js"
-    integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
-    crossorigin="anonymous"></script>
-  <script src="js/scripts.min.js"></script>
-  <script src="js/main.min.js"></script>
-  <script src="js/custom.js"></script>
-  <!-- <script src="js/jquery.min.js"></script> -->
